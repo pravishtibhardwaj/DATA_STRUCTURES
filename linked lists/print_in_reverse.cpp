99120557d0eb888083e8d2a331176794ae9b2d09
Pravishti Bhardwaj <76443753+pravishtibhardwaj@users.noreply.github.com>
2022-01-10 09:36:55 +0530

    Create print_in_reverse.cpp

---
+++ linked lists/print_in_reverse.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep) {
+    while (node) {
+        cout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            cout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*
+ * Complete the 'reversePrint' function below.
+ *
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+void reversePrint(SinglyLinkedListNode* llist) {
+    if(llist==NULL)
+    return;
+    SinglyLinkedListNode*prev=NULL;
+    SinglyLinkedListNode*cur=llist;
+    SinglyLinkedListNode*nextptr;
+    while(cur!=NULL)
+    {
+        nextptr=cur->next;
+        cur->next=prev;
+        prev=cur;
+        cur=nextptr;
+    }
+    SinglyLinkedListNode*newnode=prev;
+    while(newnode!=NULL)
+    {
+        cout<<newnode->data<<endl;
+        newnode=newnode->next;
+    }
+}
+
+int main()
