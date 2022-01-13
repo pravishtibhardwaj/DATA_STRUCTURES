3001c7d8ef63fcaab37a4f9ab8ca4c41c2398045
Pravishti Bhardwaj <76443753+pravishtibhardwaj@users.noreply.github.com>
2022-01-13 10:50:24 +0530

    Create MIN_and_MAX_of_BT_using_STACK.cpp

---
+++ binary_trees/MIN_and_MAX_of_BT_using_STACK.cpp
@@ -0,0 +1,34 @@
+
+//find minimum and maximum of given binary tree 
+
+void min_max(node*root)
+{
+	int min,max;
+	if(root==NULL)
+	return;
+	min=max=root->data;
+	queue<node*>q;
+	q.push(root);
+	while(!q.empty())
+	{
+		node*temp=q.front();
+		q.pop();
+		if(temp->lc)
+		{
+			q.push(temp->lc);
+		}
+		if(temp->rc)
+		{
+			q.push(temp->rc);
+		}
+		cout<<"\ntemp->data="<<temp->data<<endl;
+		if(temp->data<min)
+		min=temp->data;
+		else if(temp->data>max)
+		max=temp->data;
+		cout<<"min="<<min;
+		cout<<"\nmax="<<max<<endl;
+	}
+    cout<<"maximum value="<<max;
+	cout<<"\nminimum value="<<min;	
+}
