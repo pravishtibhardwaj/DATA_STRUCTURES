cddabbce437105c9d097fe9b6aaddf40dd0284ad
Pravishti Bhardwaj <76443753+pravishtibhardwaj@users.noreply.github.com>
2022-01-19 10:36:20 +0530

    Create Populate Inorder successor of all nodes.cpp

---
+++ binary_search_tress/Populate Inorder successor of all nodes.cpp
@@ -0,0 +1,22 @@
+/*Given a Binary Tree, write a function to populate next pointer for all nodes. 
+The next pointer for every node should be set to point to inorder successor.*/
+ void inorder_t(Node*root,vector<Node*>&v)
+    {
+        if(root==NULL)
+        return;
+        inorder_t(root->left,v);
+        v.push_back(root);
+        inorder_t(root->right,v);
+        
+    }
+    void populateNext(Node *root)
+    {
+        if(root==NULL)
+        return;
+        vector<Node*>v;
+        inorder_t(root,v);
+        for(int i=0;i<v.size()-1;i++)
+        v[i]->next=v[i+1];
+        v[v.size()-1]->next=NULL;
+        //code here
+    }
