80f1c549fc3fc1f42cb19ca38fd144797031c013
Pravishti Bhardwaj <76443753+pravishtibhardwaj@users.noreply.github.com>
2022-01-20 10:47:31 +0530

    Create k-th smallest element in BST.cpp

---
+++ binary_search_tress/k-th smallest element in BST.cpp
@@ -0,0 +1,18 @@
+void inorder(Node*root,vector<int>&v)
+    {
+    if(root==NULL)
+    return ;
+    inorder(root->left,v);
+    v.push_back(root->data);
+    inorder(root->right,v);
+    }
+    
+    int KthSmallestElement(Node *root, int K) {
+        // add code here.
+         vector<int>v;
+        inorder(root,v);
+        if(root==NULL || K>v.size() )
+        return -1;
+       
+        return v[K-1];
+    }
