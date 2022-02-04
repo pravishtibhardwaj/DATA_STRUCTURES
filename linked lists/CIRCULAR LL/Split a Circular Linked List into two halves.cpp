9d6d34c990898a52708d2bf96b6666e06c334df4
Pravishti Bhardwaj <76443753+pravishtibhardwaj@users.noreply.github.com>
2022-02-04 22:10:24 +0530

    Update Split a Circular Linked List into two halves.cpp

--- linked lists/CIRCULAR LL/Split a Circular Linked List into two halves.cpp
+++ linked lists/CIRCULAR LL/Split a Circular Linked List into two halves.cpp
@@ -4,11 +4,12 @@ Output:
 1 5
 7 
 */
-
+//code by pravishtibhardwaj
 void splitList(Node *head, Node **head1_ref, Node **head2_ref)
 {
     if(head==NULL || head->next==NULL)
     return;
+    
     *head1_ref=head;
     Node*slow=head,*fast=head->next;
     while(fast!=head && fast->next!=head)
