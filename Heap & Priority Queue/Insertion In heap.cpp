3b122a17e1b283ab08c668e821ac5087713a1496
Pravishti Bhardwaj <76443753+pravishtibhardwaj@users.noreply.github.com>
2022-02-18 19:40:49 +0530

    Create Insertion In heap.cpp

---
+++ Heap & Priority Queue/Insertion In heap.cpp
@@ -0,0 +1,39 @@
+// MAX Heap
+#include<iostream>
+#include<vector>
+using namespace std;
+void heapify(vector<int>&v,int i)
+{
+    int parent=(i-1)/2;
+    if(!v[parent])
+    return;
+    if(v[i]>v[parent])
+    {
+        swap(v[i],v[parent]);
+        heapify(v,parent);
+    }
+}
+void insert_node(vector<int>&v,int key)
+{
+    //inserting the element at the end of the array
+    v.push_back(key);
+
+    //since the new element is inserted at the end the properties of heap may be violated
+    //heapify the newnode
+
+    heapify(v,v.size()-1);
+}
+void print(vector<int>v)
+{
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<" ";
+}
+int main()
+{
+    //vcetor containing elements of max heap
+    vector<int>v={40,15,20,10,12,19,18};
+    insert_node(v,35);
+    print(v);
+
+
+}
